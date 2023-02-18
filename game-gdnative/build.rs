af5ddd24c2be17880a5b3c5df3e988e42569be6b
Benjamin Sherman <benjamin@bensherman.io>
2023-02-18 15:51:55 -0500

    Sort puzzles in CI

--- game-gdnative/build.rs
+++ game-gdnative/build.rs
@@ -12,6 +12,15 @@ fn modified_time(direntry: &walkdir::DirEntry) -> SystemTime {
         .unwrap_or(SystemTime::UNIX_EPOCH)
 }
 
+fn abspath(path: &Path) -> Option<String> {
+    path.canonicalize()
+        .ok()
+        .map(PathBuf::into_os_string)
+        .map(OsString::into_string)
+        .map(|x| x.ok())
+        .unwrap_or_default()
+}
+
 fn main() {
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("puzzle_definitions.rs");
@@ -25,16 +34,17 @@ fn main() {
         WalkDir::new("src/puzzles").sort_by(|d1, d2| modified_time(d2).cmp(&modified_time(d1)))
     };
 
-    let puzzles = files
+    let mut puzzles = files
         .into_iter()
         .filter_map(|d| d.ok())
         .filter(|direntry| direntry.file_type().is_file())
-        .filter_map(|file| file.path().canonicalize().ok())
-        .map(PathBuf::into_os_string)
-        .map(OsString::into_string)
-        .filter_map(|x| x.ok())
+        .filter_map(|file| abspath(file.path()))
         .filter(|x| x.ends_with(".yaml"))
         .collect::<Vec<String>>();
+
+    if std::env::var("CI").unwrap_or("false".to_string()) == "true" {
+        puzzles.sort();
+    }
 
     std::fs::write(
         &dest_path,
